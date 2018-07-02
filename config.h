44f0933a583842e612c4ed165abaf31a056ef71e
Fredrick Meunier <fredm@spamcop.net>
2018-07-02 20:39:15 +1000

    Merge in libspectrum 1.4.4 changes

--- config.h
+++ config.h
@@ -86,7 +86,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.4.3"
+#define VERSION "1.4.4"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
